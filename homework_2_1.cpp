b0cddbec9a9533983de9519021763aa75ab98d41
TitovAA <agamir.dkg@gmail.com>
2025-05-28 22:52:22 +0300

    homwork 2_1/2_2

---
+++ homework_2_1.cpp
@@ -0,0 +1,23 @@
+﻿// homework_2_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+int fibonacci(int n)
+{
+    return n < 2 ? n : fibonacci(n - 2) + fibonacci(n - 1);
+}
+
+int main() {
+    
+    std::cout << "Введите число: ";
+    int num{};
+    std::cin >> num;
+
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout << fibonacci(i) << " ";
+    }
+    return 0;
+}
---
+++ homework_2_2.cpp
@@ -0,0 +1,31 @@
+﻿// homework_2_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+long long fibonacci(int n, long long* mass)
+{
+    if (mass[n] != 0) return mass[n];
+    else if (n < 2) {
+        mass[n] = n;
+    }
+    else {
+        mass[n] = fibonacci(n - 2, mass) + fibonacci(n - 1, mass);
+    }
+    return mass[n];
+}
+
+int main() {
+    
+    std::cout << "Введите число: ";
+    int num{};
+    std::cin >> num;
+    long long* mass = new long long[num] {};
+    std::cout << std::endl;
+    for (int i = 0; i < num; ++i)
+    {
+        std::cout << fibonacci(i, mass) << " ";
+    }
+    return 0;
+}
